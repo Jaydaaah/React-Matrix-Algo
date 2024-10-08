3918a67adadbfb5932f3c861c0ba86f3d2d31522
Jay Dee <sv.jdrapha@gmail.com>
2024-10-08 16:28:04 +0800

    Fix color

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -26,7 +26,7 @@ export default function Card({ children, className = "w-full" }) {
                 duration: 0.5,
                 ease: "easeOut",
             }}
-            className={`p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 max-h-[85vh] overflow-y-auto no-scrollbar absolute ${className}`}
+            className={`p-6 border rounded-lg shadow bg-gray-800 border-gray-700 max-h-[85vh] overflow-y-auto no-scrollbar absolute ${className}`}
         >
             {children}
         </motion.div>
